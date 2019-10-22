a9e70eef328985a5ffddb24402eb831c672d83a7
Gabrielle Weiner <gweiner@ucvts.org>
2019-10-22 13:18:31 -0400

    completed exercise 3

--- src/area.js
+++ src/area.js
@@ -1,4 +1,10 @@
 const readlineSync = require("readline-sync");
 
-const width = readlineSync.question("\nWidth: ");
-const length = readlineSync.question("Length: ");
+const widthInches = readlineSync.question("\nWidth: ");
+const lengthInches = readlineSync.question("Length: ");
+const INCHES_TO_MILLIMETERS = 25.4;
+let widthMillimeters = widthInches * INCHES_TO_MILLIMETERS;
+let lengthMillimeters = lengthInches * INCHES_TO_MILLIMETERS;
+let area = widthMillimeters * lengthMillimeters;
+area = area.toLocaleString({maximumFractionDigits: 2, minimumFractionDigits: 2});
+console.log("\nA(n) " + widthInches + "-by-" + lengthInches + "-inch sheet of paper has an area of " +  area  + " square millimeter(s).");
