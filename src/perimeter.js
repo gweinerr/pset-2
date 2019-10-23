5a061580be2440076d6278223ba5dc8718b990cc
EMILY STEINER <emilysteiner@EMILYs-iMac.local>
2019-10-23 19:07:59 -0400

    finished exercise 4

---
+++ src/perimeter.js
@@ -0,0 +1,10 @@
+const readlineSync = require("readline-sync");
+
+const widthInches = readlineSync.question("\nWidth: ");
+const lengthInches = readlineSync.question("Length: ");
+const INCHES_TO_CM = 2.54;
+let widthCm = widthInches * INCHES_TO_CM;
+let lengthCm = lengthInches * INCHES_TO_CM;
+let perimeter = (2 * widthCm) + (2 * lengthCm);
+perimeter = perimeter.toLocaleString({maximumFractionDigits: 2, minimumFractionDigits: 2});
+console.log("\nA(n) " + widthInches + "-by-" + lengthInches + "-inch sheet of paper has an perimeter of " +  perimeter  + " centimeter(s).");
