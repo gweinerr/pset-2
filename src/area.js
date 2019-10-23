272294832c8d768cc4021a14f58f3b0cdeaea169
EMILY STEINER <emilysteiner@EMILYs-iMac.local>
2019-10-23 19:23:09 -0400

    corrected area.js hello-user.js and perimeter.js as well as completing exercise 5

--- src/area.js
+++ src/area.js
@@ -7,4 +7,4 @@ let widthMillimeters = widthInches * INCHES_TO_MILLIMETERS;
 let lengthMillimeters = lengthInches * INCHES_TO_MILLIMETERS;
 let area = widthMillimeters * lengthMillimeters;
 area = area.toLocaleString({maximumFractionDigits: 2, minimumFractionDigits: 2});
-console.log("\nA(n) " + widthInches + "-by-" + lengthInches + "-inch sheet of paper has an area of " +  area  + " square millimeter(s).");
+console.log("\nA(n) " + widthInches + "-by-" + lengthInches + "-inch sheet of paper has an area of " +  area  + " square millimeter(s).\n");
--- src/perimeter.js
+++ src/perimeter.js
@@ -7,4 +7,4 @@ let widthCm = widthInches * INCHES_TO_CM;
 let lengthCm = lengthInches * INCHES_TO_CM;
 let perimeter = (2 * widthCm) + (2 * lengthCm);
 perimeter = perimeter.toLocaleString({maximumFractionDigits: 2, minimumFractionDigits: 2});
-console.log("\nA(n) " + widthInches + "-by-" + lengthInches + "-inch sheet of paper has an perimeter of " +  perimeter  + " centimeter(s).");
+console.log("\nA(n) " + widthInches + "-by-" + lengthInches + "-inch sheet of paper has an perimeter of " +  perimeter  + " centimeter(s).\n");
