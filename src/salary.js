0ca95b92831ea1f932091a4f22a06dfbbe4d1581
Gabrielle Weiner <gweiner@ucvts.org>
2019-10-24 12:15:51 -0400

    finished exercise 7

--- src/salary.js
+++ src/salary.js
@@ -1,3 +1,17 @@
 const readlineSync = require("readline-sync");
 
 const hrwg = readlineSync.question("\nHourly Wage: ");
+
+const mon = readlineSync.question("\nMonday: ");
+const tues = readlineSync.question("Tuesday: ");
+const wed = readlineSync.question("Wednesday: ");
+const thurs = readlineSync.question("Thursday: ");
+const fri = readlineSync.question("Friday: ");
+const sat = readlineSync.question("Saturday: ");
+const sun = readlineSync.question("Sunday: ");
+
+let wkhr = parseFloat(mon) + parseFloat(tues) + parseFloat(wed) + parseFloat(thurs) + parseFloat(fri) + parseFloat(sat) + parseFloat(sun)
+let pay = (wkhr * hrwg)
+
+pay = pay.toLocaleString(undefined, {maximumFractionDigits: 2, minimumFractionDigits: 2});
+console.log("\nYou'll make $" + pay + " this week.");
